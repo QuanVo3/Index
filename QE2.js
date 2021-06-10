7c1ac5929108b7f50a3a8a9c7e2477f6604e6288
Quan Vo <votrandongquan@gmail.com>
2021-06-10 19:54:10 +0700

    Test

--- QE2.js
+++ QE2.js
@@ -1,3 +1,18 @@
-var str = prompt("Enter string!");
-str.charAt(0).toUpperCase();
-alert(str);
+var Input = prompt("Enter string!");
+if(Input.charCodeAt(0) > 90)
+{
+    let temp = 0;
+    temp = Input.charCodeAt(0) - 32;
+    Input[0] = toString(temp);
+}
+for(let i = 0; i < Input.length;i++)
+{
+    if(Input[i] == ' ')
+    {
+        if(Input.charCodeAt(i+1) > 90)
+        {
+            Input.charAt(i+1).toUpperCase(); 
+        }
+    }
+}
+alert(Input);
--- QE5.js
+++ QE5.js
@@ -3,12 +3,12 @@ let SplDate = date.split("/");
 let day = SplDate[0];
 let month = SplDate[1];
 let year = SplDate[2];
-if(month < 1 || month > 12 || date < 1 || date > 31)
+if(month < 1 || month > 12 || date < 1 || date > 31 || year < 1000)
 {
     alert("Invalid year!");
 }
 else{
-    if(year % 2 == 0 && year % 2 != 0)
+    if(year % 4 == 0 && year % 100 != 0)
     {
         if(month == 2 && day > 29 )
         {
